62ec8d5e9f604de5d67dfbc7cc0f890b5199630f
Tigran Melkonian <tigran96@bu.edu>
2018-07-15 19:23:32 -0400

    Update scraper.R

--- scraper.R
+++ scraper.R
@@ -3,70 +3,9 @@ library('dplyr')
 library('stringi')
 library('omdbapi')
 library('Hmisc')
-require(V8)
-library('stringr')
-require('XML')
 library('tidyverse')
 library('plyr')
 library('taRifx')
-##########################
-#The Numbers scrape - retrieving movie info from thenumbers.com
-#########################
-#Returns: A data frame containing movie titles, release date, production budget, domestic gross, and worldwide gross
-###########################
-
-search_url <- paste0('https://www.the-numbers.com',gsub(' ','%20',num))
-
-
-#Scrape Raw HTML from page
-search_results <- read_html(search_url)
-
-#pull all movie titles and their release dates for the first 5000 movies on the site
-movieName_and_releasDate <- lapply(paste0('https://www.the-numbers.com/movie/budgets/all/', seq(1,5501, 100)),
-                                   function(url){
-                                     url %>% read_html() %>% 
-                                       html_nodes("td a") %>% 
-                                       html_text() %>%
-                                       gsub('[\r\n\t]', '', .)
-                                     
-                                   })
-
-#pull production budget, domestic gross, and worldwide gross for the first 5000 movies on the site
-gross_and_budget <- lapply(paste0('https://www.the-numbers.com/movie/budgets/all/', seq(1,5501, 100)),
-                           function(url){
-                             url %>% read_html() %>% 
-                               html_nodes(".data") %>% 
-                               html_text() %>%
-                               gsub('[\r\n\t]', '', .)
-                             
-                           })
-#unlist to a vector, convert vector to dataset, assign col names
-vector1 <- unlist(movieName_and_releasDate)
-m1 <- matrix(vector1, ncol = 2, byrow = TRUE)
-d1 <- as.data.frame(m1, stringsAsFactors = FALSE)
-colnames(d1) <- 
-  c('release_date',
-    'movie_name')
-
-#unlist to a vector, convert vector to dataset, assign col names
-vector2 <- unlist(gross_and_budget)
-m2 <- matrix(vector2, ncol = 4, byrow = TRUE)
-d2 <- as.data.frame(m2, stringsAsFactors = FALSE)
-colnames(d2) <-
-  c('num',
-    'production_budget',
-    'domestic_gross',
-    'worldwide_gross')
-theNumbersData <- cbind(d1, d2)
-
-#get rid of useless col
-theNumbersData <- theNumbersData[-3]
-
-#export dataset as csv
-write.csv(theNumbersData, "theNumbersData.csv")
-
-
-
 
 #############################
 ###IMDB SCRAPE - retirieving movie info from IMDB.com
@@ -218,7 +157,16 @@ get_movie_data <- function(url){
   
   
   
-  data.frame(title[1], toString(generes), toString(actors), toString(directors),length, mpaa, imdbScore, metascore,numVotes, aspectRatio)
+  data.frame(title[1],
+             toString(generes), 
+             toString(actors), 
+             toString(directors),
+             length, 
+             mpaa, 
+             imdbScore,
+             metascore,
+             numVotes, 
+             aspectRatio)
 }
 
 #get movie data for each moive url in imdbURL
@@ -234,14 +182,24 @@ df<- ldply(datalist, data.frame)
 #get rid of duplicates
 dfClean <- unique(df)
 
+#convert title to a string and then get rid of extra white space
 dfClean$title <- as.character(dfClean$title)
 trim.trailing <- function (x) sub("\\s+$", "", x)
 for (i in 1:nrow(dfClean)) {
   dfClean$title[i]<- trim.trailing(dfClean$title[i])
 }
+
+
 #add on financial details from theNumbers Data
 theNumbersData <- theNumbersData[, c(2, 1, 3, 4, 5)]
 df_final <- merge(dfClean, theNumbersData, by = c("title"))
+
 #rename columns
-colnames(df_final) <- c("title", "genere(s)", "actors", "directors", "length", "mpaa", "imdbScore", "metaScore", "numVotes", "aspectRatio", "releaseDate", "productionBudget", "domesticGross", "worldWideGross")
+colnames(df_final) <- c("title", "genere(s)",
+                        "actors", "directors", 
+                        "length", "mpaa", 
+                        "imdbScore", "metaScore",
+                        "numVotes", "aspectRatio",
+                        "releaseDate", "productionBudget", 
+                        "domesticGross", "worldWideGross")
 
