cd4b05d4e0ca128efae42d3f6543f4dbb1172d46
Tigran Melkonian <tigran96@bu.edu>
2018-07-16 16:56:07 -0400

    Update IMDBscrape.R

--- IMDBscrape.R
+++ IMDBscrape.R
@@ -18,163 +18,141 @@ library('taRifx')
 omdbKey <-  'SECRET_KEY'
 omdb_api_key(force = FALSE)
 
-#retrieve imdb ID for each movie scraped from theNumbers
-get_movieID <- function(movie_title) {
-  search_return <- search_by_title(movie_title, api_key = omdbKey)
-  if(length(search_return$imdbID) >= 1 && !is.na(search_return)){
-    id <- (search_return$imdbID[1])
-  }else{
-    id <- NA
-  }
-  
-  return(id)
-}
-
-#charcter list for movieID's
-movieID <- character()
-
-#loop over all the movie names and retrieve each imdb id
-for (i in 1:nrow(theNumbersData)) {
-  movieID[i] <- get_movieID(theNumbersData$Title[i])
-}
-## removing all NA's from the list to make it easier to process IDs
-movieID <- movieID[!is.na(movieID)]
-
-
-#create imdb url for 5282 movies
-for (i in 1:length(movieID)){
-  imdbURL <- paste0("https://www.imdb.com/title/",gsub('%20',movieID[i],movieID))
-}
-
-#save all imdb urls as csv
-write.csv(imdbURL, "movie_imdb_url.csv")
-
-
-##get movie details 
-get_movie_data <- function(url){
-  
+##get movie details
+get_movie_data <- function(url) {
   html <- read_html(url)
   
-   #title of movie
-  title <- tryCatch({html%>%
-    html_nodes(xpath = '//div[@class="title_wrapper"]/h1/text()') %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  #title of movie
+  title <- tryCatch({
+    html %>%
+      html_nodes(xpath = '//div[@class="title_wrapper"]/h1/text()') %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   #length of movie in minutes
-  length <- tryCatch({html%>%
-    html_nodes("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time") %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  length <- tryCatch({
+    html %>%
+      html_nodes(
+        "#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time"
+      ) %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   #find MPAA
-  mpaa <- tryCatch({html%>%
-    html_nodes("#titleStoryLine > div:nth-child(12) > span:nth-child(2)") %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)%>%
-    strsplit(mpaa, split = " ", fixed = TRUE)%>%
-    unlist(mpaa)
-  }, error=function(cond)
-    {
+  mpaa <- tryCatch({
+    html %>%
+      html_nodes("#titleStoryLine > div:nth-child(12) > span:nth-child(2)") %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .) %>%
+      strsplit(mpaa, split = " ", fixed = TRUE) %>%
+      unlist(mpaa)
+  }, error = function(cond)
+  {
     NA
   })
   
   mpaa <- mpaa[2]
   
   #Find genere(s)
-  generes <- tryCatch({html%>%
-    html_nodes('.subtext .itemprop') %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  generes <- tryCatch({
+    html %>%
+      html_nodes('.subtext .itemprop') %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   
   #find director(S)
-  directors <- tryCatch({html%>%
-    html_nodes('.credit_summary_item:nth-child(2) .itemprop') %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  directors <- tryCatch({
+    html %>%
+      html_nodes('.credit_summary_item:nth-child(2) .itemprop') %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   #find top iactor(s)
-  actors <- tryCatch({html%>%
-    html_nodes(  '.credit_summary_item:nth-child(4) .itemprop') %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  actors <- tryCatch({
+    html %>%
+      html_nodes('.credit_summary_item:nth-child(4) .itemprop') %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   #number of votes
-  numVotes <- tryCatch({html%>%
-    html_nodes("a .small") %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  numVotes <- tryCatch({
+    html %>%
+      html_nodes("a .small") %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   #find imdb score
-  imdbScore <- tryCatch({html%>%
-    html_nodes("strong span") %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  imdbScore <- tryCatch({
+    html %>%
+      html_nodes("strong span") %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   #find metascore
-  metascore <- tryCatch({html%>%
-    html_nodes("div.titleReviewBar > div:nth-child(1) > a > div") %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  metascore <- tryCatch({
+    html %>%
+      html_nodes("div.titleReviewBar > div:nth-child(1) > a > div") %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
   
   #find aspect ratio
-  aspectRatio <- tryCatch({html%>%
-    html_nodes(xpath = '//h4[contains(text(), "Aspect Ratio:")]/following-sibling::node()/descendant-or-self::text()') %>%
-    html_text()%>%
-    gsub('[\r\n\t]', '', .)
-  }, error=function(cond)
+  aspectRatio <- tryCatch({
+    html %>%
+      html_nodes(xpath = '//h4[contains(text(), "Aspect Ratio:")]/following-sibling::node()/descendant-or-self::text()') %>%
+      html_text() %>%
+      gsub('[\r\n\t]', '', .)
+  }, error = function(cond)
   {
     NA
   })
   
-  
-  
-  data.frame(title[1],
-             toString(generes), 
-             toString(actors), 
-             toString(directors),
-             length, 
-             mpaa, 
-             imdbScore,
-             metascore,
-             numVotes, 
-             aspectRatio)
+  data.frame(
+    title[1],
+    toString(generes),
+    toString(actors),
+    toString(directors),
+    length,
+    mpaa,
+    imdbScore,
+    metascore,
+    numVotes,
+    aspectRatio
+  )
 }
-
 #get movie data for each moive url in imdbURL
 datalist <- list()
 for (i in 1:length(imdbURL)) {
